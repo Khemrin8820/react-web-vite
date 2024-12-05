50d7700cf0d01ba4179060d2cf27bfe85970add8
Khemrin <senkhemrin@gmail.com>
2024-12-05 21:32:27 +0700

    v1.0.1

--- react-web-vite/src/App.jsx
+++ react-web-vite/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import ProductPage from './pages/product/ProductPage';
 import HomePage from './pages/home/HomePage';
+import AboutPage from './pages/about/AboutPage';
 
 function App() {
 
@@ -9,7 +10,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        <Route path="/product" element={<ProductPage/>}/>
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/login" element={<ProductPage />} />
+        <Route path="/register" element={<ProductPage/>}/>
       </Routes>
     </BrowserRouter>
   );
