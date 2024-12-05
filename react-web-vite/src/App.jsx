40005aff76077f6cf64065cab2ccd695ec9e4c0a
Khemrin <senkhemrin@gmail.com>
2024-12-05 15:40:03 +0700

    v1.0.1

--- react-web-vite/src/App.jsx
+++ react-web-vite/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ProductPage from './pages/product/ProductPage';
+import HomePage from './pages/home/HomePage';
 
 function App() {
 
   return (
-    <div>
-      This is App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/product" element={<ProductPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
